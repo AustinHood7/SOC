61691b6a61d38188c7fd06e73de196351499605d
Hailey DLS <hailey.dls@hotmail.com>
2024-04-05 19:41:42 -0400

    second commit

--- pages/Room1.jsx
+++ pages/Room1.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Room1 from "./Room1.jsx"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <Room1 />
+    <>
+        <div>Hello World</div>
+    </>
   );
 }
